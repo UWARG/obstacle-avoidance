908cecaa88980aa67dc02bc9f1a916c35958c91f
TongguangZhang <tonggz250@gmail.com>
2024-05-27 09:46:06 -0400

    added basic test

---
+++ tests/unit/test_sample.py
@@ -0,0 +1,22 @@
+"""
+Sample test class, delete when tests are written
+"""
+
+import pytest 
+
+class TestSample:
+    """
+    This class runs a basic test
+    """
+
+    def test_add(self):
+        """
+        Test addition
+        """      
+        input_1 = 1
+        input_2 = 2
+        expected = 3
+
+        actual = input_1 + input_2
+
+        assert actual == expected
