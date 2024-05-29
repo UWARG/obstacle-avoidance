2f82a1bb9d1904cb8c6f571e4df776054d5c092a
Andrew Shum <andrew.shum068@gmail.com>
2024-05-29 15:27:01 -0400

    added multiprocessing worker and conversion methods

--- modules/flight_interface/flight_interface.py
+++ modules/flight_interface/flight_interface.py
@@ -1,34 +1,38 @@
-
-from . import conversions # to be implemented
+from . import conversions
 from .. import drone_odometry_local
 
 from ..common.mavlink.modules import drone_odometry
 from ..common.mavlink.modules import flight_controller
 
+
 class FlightInterface:
+    """
+    Create flight controller and sets home location
+    """
 
     __create_key = object()
 
-    def create(
-        cls,
-        address: str,
-        timeout_home: float
-    ) -> "tuple[bool, FlightInterface | None]":
+    @classmethod
+    def create(cls, address: str, timeout_home: float) -> "tuple[bool, FlightInterface | None]":
+        """
+        address: TCP address or port.
+        timeout_home: Timeout for home location in seconds.
+        """
         result, controller = flight_controller.FlightController.create(address)
         if not result:
             return False, None
-        
+
         result, home_location = controller.get_home_location(timeout_home)
         if not result:
             return False, None
-        
+
         return True, FlightInterface(cls.__create_key, controller, home_location)
 
     def __init__(
-        self, 
+        self,
         create_key: object,
         controller: flight_controller.FlightController,
-        home_location: drone_odometry.DroneLocation, 
+        home_location: drone_odometry.DroneLocation,
     ) -> None:
         assert create_key is FlightInterface.__create_key, "Use create() method"
 
@@ -36,18 +40,19 @@ class FlightInterface:
         self.home_location = home_location
 
     def run(self) -> "tuple[bool, drone_odometry_local.DroneOdometryLocal | None]":
-
-        result, drone_odometry = self.controller.get_odometry()
+        """
+        Returns local drone odometry with timestamp
+        """
+        result, odometry = self.controller.get_odometry()
         if not result:
             return False, None
 
-        drone_position = drone_odometry.position
-        
-        result, drone_position_local = conversions.global_to_local(drone_position, self.home_location)
+        global_position = odometry.position
+
+        result, local_position = conversions.global_to_local(global_position, self.home_location)
         if not result:
             return False, None
 
-        drone_orientation = drone_odometry.orientation
+        drone_orientation = odometry.orientation
 
-        return drone_odometry_local.DroneOdometryLocal.create(drone_position_local, drone_orientation)
-    
+        return drone_odometry_local.DroneOdometryLocal.create(local_position, drone_orientation)
