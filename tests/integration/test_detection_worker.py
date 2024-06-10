b26d77896b363721d90291d061c028b5cad57dab
Andrew Shum <andrew.shum068@gmail.com>
2024-06-10 15:58:28 -0400

    debugging and added inputs for lidar configuration

--- tests/integration/test_detection_worker.py
+++ tests/integration/test_detection_worker.py
@@ -11,7 +11,7 @@ from worker import queue_wrapper
 from modules import drone_odometry_local
 from modules import detection_and_odometry
 from modules.detection import detection_worker
-from ..common.mavlink.modules import drone_odometry
+from modules.common.mavlink.modules import drone_odometry
 
 # Constants
 WORK_COUNT = 3
@@ -20,6 +20,10 @@ DELAY_FOR_SIMULATED_WORKER = 1  # seconds
 SERIAL_PORT_NAME = "/dev/tty.usbmodem38S45_158681"
 SERIAL_PORT_BAUDRATE = 921600
 PORT_TIMEOUT = 0.1  # seconds
+UPDATE_RATE = 5
+HIGH_ANGLE = 170
+LOW_ANGLE = -170
+ROTATE_SPEED = 5
 
 
 def simulate_previous_worker(in_queue):
@@ -57,6 +61,10 @@ def main():
             SERIAL_PORT_NAME,
             SERIAL_PORT_BAUDRATE,
             PORT_TIMEOUT,
+            UPDATE_RATE,
+            HIGH_ANGLE,
+            LOW_ANGLE,
+            ROTATE_SPEED,
             odometry_in_queue,
             detection_out_queue,
             controller,
@@ -71,7 +79,7 @@ def main():
     time.sleep(DELAY_FOR_SIMULATED_WORKER)
 
     for _ in range(0, WORK_COUNT):
-        input_data: detection_and_odometry.DetectionAndOdometry = detection_out_queue.get_nowait()
+        input_data: detection_and_odometry.DetectionAndOdometry = detection_out_queue.queue.get_nowait()
         print(input_data)
         assert input_data is not None
 
