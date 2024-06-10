d3c74bf4cf6aec52dd984e198b1e7138227947b7
Andrew Shum <andrew.shum068@gmail.com>
2024-06-10 15:57:07 -0400

    added units and better parameter name

--- modules/detection/detection.py
+++ modules/detection/detection.py
@@ -18,23 +18,23 @@ class Detection:
         serial_port_baudrate: int,
         timeout: float,
         update_rate: int,
-        low_angle: int,
-        high_angle: int,
-        speed: int,
+        low_angle: float,
+        high_angle: float,
+        rotate_speed: int,
     ) -> None:
         """
         serial_port_name: port that the lidar is connected to
         serial_port_baudrate: baudrate of the lidar port
         timeout: timeout for connecting to serial port
-        update_rate: frequency the lidar reads points (must be between 1 and 12 inclusive)
-        low_angle: lidar low angle (must be between -170 and -5 inclusive)
-        high_angle: lidar high angle (must be between 5 and 170 inclusive)
-        speed: lidar rotational speed (must be between 5 and 2000 inclusive where 5 is the fastest)
+        update_rate: frequency the lidar reads points (must be integer between 1 and 12 inclusive)
+        low_angle: lidar low angle in degrees (must be between -170 and -5 inclusive)
+        high_angle: lidar high angle in degrees (must be between 5 and 170 inclusive)
+        speed: lidar rotational speed (must be integer between 5 and 2000 inclusive where 5 is the fastest)
         """
         self.update_rate = update_rate
         self.low_angle = low_angle
         self.high_angle = high_angle
-        self.speed = speed
+        self.rotate_speed = rotate_speed
 
         self.lidar = lidar_driver.LidarDriver(serial_port_name, serial_port_baudrate, timeout)
 
@@ -43,7 +43,7 @@ class Detection:
         self.lidar.set_default_distance_output(self.lidar.serial_port)
         self.lidar.set_low_angle(self.lidar.serial_port, self.low_angle)
         self.lidar.set_high_angle(self.lidar.serial_port, self.high_angle)
-        self.lidar.set_speed(self.lidar.serial_port, self.speed)
+        self.lidar.set_speed(self.lidar.serial_port, self.rotate_speed)
 
         # start streaming
         self.lidar.set_default_distance_output(self.lidar.serial_port)
