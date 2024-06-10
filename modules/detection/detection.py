58d85d2b1de295c6ee5d652922e90788226ddbfa
Andrew Shum <andrew.shum068@gmail.com>
2024-06-10 04:47:41 -0400

    added lidar configs as inputs to init method

--- modules/detection/detection.py
+++ modules/detection/detection.py
@@ -12,20 +12,38 @@ class Detection:
     Configures and receives lidar data
     """
 
-    def __init__(self, serial_port_name: str, serial_port_baudrate: int, timeout: float) -> None:
+    def __init__(
+        self,
+        serial_port_name: str,
+        serial_port_baudrate: int,
+        timeout: float,
+        update_rate: int,
+        low_angle: int,
+        high_angle: int,
+        speed: int,
+    ) -> None:
         """
         serial_port_name: port that the lidar is connected to
         serial_port_baudrate: baudrate of the lidar port
         timeout: timeout for connecting to serial port
+        update_rate: frequency the lidar reads points (must be between 1 and 12 inclusive)
+        low_angle: lidar low angle (must be between -170 and -5 inclusive)
+        high_angle: lidar high angle (must be between 5 and 170 inclusive)
+        speed: lidar rotational speed (must be between 5 and 2000 inclusive where 5 is the fastest)
         """
+        self.update_rate = update_rate
+        self.low_angle = low_angle
+        self.high_angle = high_angle
+        self.speed = speed
+
         self.lidar = lidar_driver.LidarDriver(serial_port_name, serial_port_baudrate, timeout)
 
         # configure lidar
-        self.lidar.set_update_rate(self.lidar.serial_port, 1)
+        self.lidar.set_update_rate(self.lidar.serial_port, self.update_rate)
         self.lidar.set_default_distance_output(self.lidar.serial_port)
-        self.lidar.set_low_angle(self.lidar.serial_port, -170)
-        self.lidar.set_high_angle(self.lidar.serial_port, 5)
-        self.lidar.set_speed(self.lidar.serial_port, 5)
+        self.lidar.set_low_angle(self.lidar.serial_port, self.low_angle)
+        self.lidar.set_high_angle(self.lidar.serial_port, self.high_angle)
+        self.lidar.set_speed(self.lidar.serial_port, self.speed)
 
     def run(
         self, odometry: drone_odometry_local.DroneOdometryLocal
@@ -37,7 +55,7 @@ class Detection:
         if odometry is None:
             return False, None
 
-        distance, angle = self.lidar.wait_for_reading(self.sensor_port)
+        distance, angle = self.lidar.wait_for_reading(self.lidar.serial_port)
 
         result, lidar_detection = detection_and_odometry.LidarDetection.create(distance, angle)
         if not result:
