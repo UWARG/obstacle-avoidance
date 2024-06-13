0367103bb5846c7c323142f929ef9d052f9fc003
Andrew Shum <84476051+ashum68@users.noreply.github.com>
2024-06-13 11:16:25 -0400

    reformatted DetectionsAndOdometry to contain a list of detections (#13)
    * reformatted DetectionsAndOdometry to contain a list of detections
    
    * changed file name and string representation
    
    * removed accidental file add

---
+++ modules/detections_and_odometry.py
@@ -0,0 +1,52 @@
+"""
+LiDAR detection and local odometry data structure.
+"""
+
+from . import drone_odometry_local
+from . import lidar_detection
+
+
+class DetectionsAndOdometry:
+    """
+    Contains LiDAR readings and current local odometry.
+    """
+
+    __create_key = object()
+
+    @classmethod
+    def create(
+        cls,
+        detections: "list[lidar_detection.LidarDetection]",
+        local_odometry: drone_odometry_local.DroneOdometryLocal,
+    ) -> "tuple[bool, DetectionsAndOdometry | None]":
+        """
+        Combines lidar readings with local odometry
+        """
+        if len(detections) == 0:
+            return False, None
+
+        if local_odometry is None:
+            return False, None
+
+        return True, DetectionsAndOdometry(cls.__create_key, detections, local_odometry)
+
+    def __init__(
+        self,
+        create_key: object,
+        detections: "list[lidar_detection.LidarDetection]",
+        local_odometry: drone_odometry_local.DroneOdometryLocal,
+    ) -> None:
+        """
+        Private constructor, use create() method.
+        """
+        assert create_key is DetectionsAndOdometry.__create_key, "Use create() method"
+
+        self.detections = detections
+        self.odometry = local_odometry
+
+    def __str__(self) -> str:
+        """
+        String representation.
+        """
+        detections_str = ", ".join(str(detection) for detection in self.detections)
+        return f"{self.__class__.__name__}, Detections ({len(self.detections)}): {detections_str}, str{self.odometry}"
