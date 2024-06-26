5849b0b543fbb84450880ea02691d503207dafca
Andrew Shum <andrew.shum068@gmail.com>
2024-06-26 03:32:09 -0400

    updated main file for simple decision

--- main.py
+++ main.py
@@ -4,14 +4,17 @@ Main
 
 import multiprocessing as mp
 
-from worker import worker_controller
-from worker import queue_wrapper
-
+from modules import decision_command
+from modules import detections_and_odometry
 from modules import drone_odometry_local
 from modules import lidar_detection
-
-from modules.flight_interface import flight_interface_worker
+from modules.data_merge import data_merge_worker
+from modules.decision import decision
+from modules.decision import decision_worker
 from modules.detection import detection_worker
+from modules.flight_interface import flight_interface_worker
+from worker import queue_wrapper
+from worker import worker_controller
 
 
 def main() -> int:
@@ -34,14 +37,20 @@ def main() -> int:
     HIGH_ANGLE = 170
     LOW_ANGLE = -170
     ROTATE_SPEED = 5
+
+    DELAY = 0.1
+
+    INITIAL_DRONE_STATE = decision.Decision.DroneState.MOVING
+    OBJECT_PROXIMITY_LIMIT = 10  # metres
     # pylint: enable=invalid-name
 
     controller = worker_controller.WorkerController()
-    manager = mp.Manager()
+    mp_manager = mp.Manager()
 
-    flight_interface_to_main_queue = queue_wrapper.QueueWrapper(manager, QUEUE_MAX_SIZE)
-
-    detection_to_main_queue = queue_wrapper.QueueWrapper(manager, QUEUE_MAX_SIZE)
+    flight_interface_to_data_merge_queue = queue_wrapper.QueueWrapper(mp_manager, QUEUE_MAX_SIZE)
+    detection_to_data_merge_queue = queue_wrapper.QueueWrapper(mp_manager, QUEUE_MAX_SIZE)
+    merged_to_decision_queue = queue_wrapper.QueueWrapper(mp_manager, QUEUE_MAX_SIZE)
+    command_to_flight_interface_queue = queue_wrapper.QueueWrapper(mp_manager, QUEUE_MAX_SIZE)
 
     flight_interface_process = mp.Process(
         target=flight_interface_worker.flight_interface_worker,
@@ -49,7 +58,8 @@ def main() -> int:
             FLIGHT_INTERFACE_ADDRESS,
             FLIGHT_INTERFACE_TIMEOUT,
             FLIGHT_INTERFACE_WORKER_PERIOD,
-            flight_interface_to_main_queue,
+            command_to_flight_interface_queue,
+            flight_interface_to_data_merge_queue,
             controller,
         ),
     )
@@ -64,39 +74,77 @@ def main() -> int:
             HIGH_ANGLE,
             LOW_ANGLE,
             ROTATE_SPEED,
-            detection_to_main_queue,
+            detection_to_data_merge_queue,
+            controller,
+        ),
+    )
+
+    data_merge_process = mp.Process(
+        target=data_merge_worker.data_merge_worker,
+        args=(
+            DELAY,
+            flight_interface_to_data_merge_queue,
+            detection_to_data_merge_queue,
+            merged_to_decision_queue,
+            controller,
+        ),
+    )
+
+    decision_process = mp.Process(
+        target=decision_worker.decision_worker,
+        args=(
+            INITIAL_DRONE_STATE,
+            OBJECT_PROXIMITY_LIMIT,
+            merged_to_decision_queue,
+            command_to_flight_interface_queue,
             controller,
         ),
     )
 
     flight_interface_process.start()
     detection_process.start()
+    data_merge_process.start()
+    decision_process.start()
 
     while True:
         try:
             flight_interface_data: drone_odometry_local.DroneOdometryLocal = (
-                flight_interface_to_main_queue.queue.get_nowait()
+                flight_interface_to_data_merge_queue.queue.get_nowait()
             )
-
             if flight_interface_data is not None:
                 print(flight_interface_data)
 
             detection_data: lidar_detection.LidarDetection = (
-                detection_to_main_queue.queue.get_nowait()
+                detection_to_data_merge_queue.queue.get_nowait()
             )
-
             if detection_data is not None:
                 print(detection_data)
+
+            merged_data: detections_and_odometry.DetectionsAndOdometry = (
+                merged_to_decision_queue.queue.get_nowait()
+            )
+            if merged_data is not None:
+                print(merged_data)
+
+            command_data: decision_command.DecisionCommand = (
+                command_to_flight_interface_queue.queue.get_nowait()
+            )
+            if command_data is not None:
+                print(command_data)
 
         except KeyboardInterrupt:
             controller.request_exit()
             break
 
-    flight_interface_to_main_queue.fill_and_drain_queue()
-    detection_to_main_queue.fill_and_drain_queue()
+    flight_interface_to_data_merge_queue.fill_and_drain_queue()
+    detection_to_data_merge_queue.fill_and_drain_queue()
+    merged_to_decision_queue.fill_and_drain_queue()
+    command_to_flight_interface_queue.fill_and_drain_queue()
 
     flight_interface_process.join()
     detection_process.join()
+    data_merge_process.join()
+    decision_process.join()
 
     return 0
 
