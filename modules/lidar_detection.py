97247aa8f68b800bc3299b06e59c991d5f46cc65
Andrew Shum <andrew.shum068@gmail.com>
2024-07-26 16:15:07 -0400

    new cluster data struct and converted lidar detection to have x, y coords instead of dist, angle

--- modules/lidar_detection.py
+++ modules/lidar_detection.py
@@ -5,39 +5,30 @@ LiDAR detection data structure.
 
 class LidarDetection:
     """
-    Lidar scan
+    Lidar scan containing point relative to drone's position.
     """
 
     __create_key = object()
 
-    __DISTANCE_LIMIT = 50
-    __ANGLE_LIMIT = 170
-
     @classmethod
-    def create(cls, distance: float, angle: float) -> "tuple[bool, LidarDetection | None]":
+    def create(cls, x: float, y: float) -> "tuple[bool, LidarDetection | None]":
         """
-        Distance is in meters.
-        Angle is in degrees.
+        x is in meters.
+        y is in meters.
         """
-        if distance < 0 or distance > cls.__DISTANCE_LIMIT:
-            return False, None
-
-        if abs(angle) > cls.__ANGLE_LIMIT:
-            return False, None
-
-        return True, LidarDetection(cls.__create_key, distance, angle)
-
-    def __init__(self, create_key: object, distance: float, angle: float) -> None:
+        return True, LidarDetection(cls.__create_key, x, y)
+    
+    def __init__(self, create_key: object, x: float, y: float) -> None:
         """
         Private constructor, use create() method.
         """
         assert create_key is LidarDetection.__create_key, "Use create() method"
 
-        self.distance = distance
-        self.angle = angle
+        self.x = x
+        self.y = y
 
     def __str__(self) -> str:
         """
-        String representation
+        String representation.
         """
-        return f"{self.__class__.__name__}: Distance: {self.distance}, Angle: {self.angle}. "
+        return f"{self.__class__.__name__}: x: {self.x}, y: {self.y}. "
