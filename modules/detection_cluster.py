60c8f9a79bb8c58d3e4af221d1c819c44fa92ef4
Andrew Shum <andrew.shum068@gmail.com>
2024-07-26 16:15:53 -0400

    formatting

--- modules/detection_cluster.py
+++ modules/detection_cluster.py
@@ -4,6 +4,7 @@ LiDAR detection cluster data structure.
 
 from . import lidar_detection
 
+
 class DetectionCluster:
     """
     Cluster of LiDAR detections.
@@ -12,13 +13,17 @@ class DetectionCluster:
     __create_key = object()
 
     @classmethod
-    def create(cls, detections: "list[lidar_detection.LidarDetection]") -> "tuple[bool, DetectionCluster | None]":
+    def create(
+        cls, detections: "list[lidar_detection.LidarDetection]"
+    ) -> "tuple[bool, DetectionCluster | None]":
         """
         Combines lidar readings in close proximity.
         """
         return True, DetectionCluster(cls.__create_key, detections)
-    
-    def __init__(self, create_key: object, detections: "list[lidar_detection.LidarDetection]") -> None:
+
+    def __init__(
+        self, create_key: object, detections: "list[lidar_detection.LidarDetection]"
+    ) -> None:
         """
         Private constructor, use create() method.
         """
