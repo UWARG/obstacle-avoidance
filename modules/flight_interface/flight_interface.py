ae617dd17a8c371d5bd9357b2e1c8d0f37b7daca
Andrew Shum <andrew.shum068@gmail.com>
2024-08-08 00:15:02 -0400

    implemented start oa on first waypoint

--- modules/flight_interface/flight_interface.py
+++ modules/flight_interface/flight_interface.py
@@ -2,6 +2,8 @@
 Creates flight controller and produces local drone odometry coupled with a timestamp.
 """
 
+import math
+
 from modules import decision_command
 from modules import drone_odometry_local
 from ..common.mavlink.modules import drone_odometry
@@ -17,7 +19,7 @@ class FlightInterface:
     __create_key = object()
 
     @classmethod
-    def create(cls, address: str, timeout_home: float) -> "tuple[bool, FlightInterface | None]":
+    def create(cls, address: str, timeout_home: float, first_waypoint_distance_tolerance: float) -> "tuple[bool, FlightInterface | None]":
         """
         address: TCP address or port.
         timeout_home: Timeout for home location in seconds.
@@ -29,14 +31,20 @@ class FlightInterface:
         result, home_location = controller.get_home_location(timeout_home)
         if not result:
             return False, None
+        
+        result, first_waypoint = controller.get_next_waypoint()
+        if not result:
+            return False, None
 
-        return True, FlightInterface(cls.__create_key, controller, home_location)
+        return True, FlightInterface(cls.__create_key, controller, home_location, first_waypoint, first_waypoint_distance_tolerance)
 
     def __init__(
         self,
         create_key: object,
         controller: flight_controller.FlightController,
         home_location: drone_odometry.DronePosition,
+        first_waypoint: drone_odometry.DroneWaypoint,
+        first_waypoint_distance_tolerance: float,
     ) -> None:
         """
         Private constructor, use create() method.
@@ -46,6 +54,16 @@ class FlightInterface:
 
         self.controller = controller
         self.home_location = home_location
+        self.first_waypoint = first_waypoint
+        self.first_waypoint_distance_tolerance = first_waypoint_distance_tolerance
+        self.__run = False
+
+    def __distance_to_first_waypoint(self, global_position: drone_odometry.DronePosition):
+        delta_x = global_position.latitude - self.first_waypoint.latitude
+        delta_y = global_position.longitude - self.first_waypoint.longitude
+        delta_z = global_position.altitude - self.first_waypoint.altitude
+        return math.sqrt(delta_x ** 2 + delta_y ** 2 + delta_z ** 2)
+
 
     def run(self) -> "tuple[bool, drone_odometry_local.DroneOdometryLocal | None]":
         """
@@ -71,6 +89,11 @@ class FlightInterface:
 
         flight_mode = drone_odometry_local.FlightMode(flight_mode.value)
 
+        distance_to_first_waypoint = self.__distance_to_first_waypoint(global_position)
+        if distance_to_first_waypoint < self.first_waypoint_distance_tolerance:
+            self.__run = True
+            print("obstacle avoidance started!")
+
         return drone_odometry_local.DroneOdometryLocal.create(
             local_position, drone_orientation, flight_mode
         )
@@ -79,6 +102,8 @@ class FlightInterface:
         """
         Uploads decision commands to drone.
         """
+        if not self.__run:
+            return False
         if command.command == decision_command.DecisionCommand.CommandType.RESUME_MISSION:
             return self.resume_handler()
         if command.command == decision_command.DecisionCommand.CommandType.STOP_MISSION_AND_HALT:
