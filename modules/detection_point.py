e90ec68ed802e1b9797923207e2732a0aa3af541
Andrew Shum <84476051+ashum68@users.noreply.github.com>
2024-08-20 15:26:53 -0400

    created detection point data structure (#38)
    * created detection point data structure
    
    * type annotation
    
    * added better description

---
+++ modules/detection_point.py
@@ -0,0 +1,36 @@
+"""
+LiDAR detection local coordinates data structure.
+"""
+
+
+class DetectionPoint:
+    """
+    Local coordinates of a LiDAR detection.
+    """
+
+    __create_key = object()
+
+    @classmethod
+    def create(cls, x: float, y: float) -> "tuple[bool, DetectionPoint]":
+        """
+        (x, y) are the local NED coordinates of the LiDAR detection relative to the drone's home position.
+        x is the x-coordinate of the LiDAR detection in metres.
+        y is the y-coordinate of the LiDAR detection in metres.
+        """
+
+        return True, DetectionPoint(cls.__create_key, x, y)
+
+    def __init__(self, create_key: object, x: float, y: float) -> None:
+        """
+        Private constructor, use create() method.
+        """
+        assert create_key is DetectionPoint.__create_key, "Use create() method"
+
+        self.x = x
+        self.y = y
+
+    def __str__(self) -> str:
+        """
+        String representation.
+        """
+        return f"{self.__class__.__name__}: x: {self.x}, y: {self.y}. "
