9789b82c357d66293a9979485dba9f23b54d6f64
Vyomm Khanna <v6khanna@uwaterloo.ca>
2024-12-04 02:26:32 -0500

    missed a file

--- modules/obstacles_and_odometry.py
+++ modules/obstacles_and_odometry.py
@@ -2,8 +2,8 @@
 Obstacles and local odometry merged data structure.
 """
 
-from . import drone_odometry_local
 from . import obstacle
+from modules import odometry_and_waypoint
 
 
 class ObstaclesAndOdometry:
@@ -17,7 +17,7 @@ class ObstaclesAndOdometry:
     def create(
         cls,
         obstacles: "list[obstacle.Obstacle]",
-        local_odometry: drone_odometry_local.DroneOdometryLocal,
+        local_odometry: odometry_and_waypoint.OdometryAndWaypoint,
     ) -> "tuple[bool, ObstaclesAndOdometry | None]":
         """
         Combines obstacles with local odometry.
@@ -31,7 +31,7 @@ class ObstaclesAndOdometry:
         self,
         create_key: object,
         obstacles: "list[obstacle.Obstacle]",
-        local_odometry: drone_odometry_local.DroneOdometryLocal,
+        local_odometry: odometry_and_waypoint.OdometryAndWaypoint,
     ) -> None:
         """
         Private constructor, use create() method.
